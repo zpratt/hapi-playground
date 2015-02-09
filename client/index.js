9fd5f43f0e27ed9f91d8ed23062a94c2945fd853
zach pratt <zach@attackofzach.com>
2015-02-08 19:37:15 -0600

    Get the list of friends and render the friend list view.

--- client/index.js
+++ client/index.js
@@ -1,5 +1,32 @@
 (function () {
     'use strict';
 
-    console.log('hey it worked');
+    var FriendsListView = require('./friend-list'),
+
+        traverson = require('traverson'),
+        React = require('react');
+
+    function renderListView(friends) {
+        React.render(
+            React.createElement(FriendsListView, {
+                friends: friends
+            }),
+            document.querySelector('.friend-list')
+        );
+    }
+
+    function init() {
+        var api = traverson.jsonHal.from('/');
+
+        api.newRequest()
+            .follow('tkr:friends', 'tkr:enemies')
+            //.withTemplateParameters({id: ''})
+            .getResource(function (err, res) {
+                var friends = [res[0], res[1]];
+
+                renderListView(friends);
+            });
+    }
+
+    init();
 }());
